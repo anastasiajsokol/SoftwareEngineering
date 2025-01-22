b84eb71920739518488462b04c92780f9e748931
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2025-01-21 19:55:53 -0700

    start public safety class

---
+++ CO4A/src/PublicSafety.java
@@ -0,0 +1,37 @@
+/**
+ *  PublicSafety class
+ *  
+ *  @author Anastasia Sokol
+ *  @author Gray Willits
+ * 
+ *  @date January 21, 2025
+**/
+
+import java.util.Arrays;
+
+public class PublicSafety {
+    private Station cityStation;
+    private Station univerityStation;
+
+    /**
+     *  Constructor accepts the names of two stations
+     * 
+     *  @param cityStationName name of the city station
+     *  @param universityStationName name of the university station
+    **/
+    public PublicSafety(String cityStationName, String universityStationName){
+        cityStation = new Station(cityStationName);
+        univerityStation = new Station(universityStationName);
+    }
+
+    /**
+     *  Prints all detectives working at public safety office
+    **/
+    public void printDetectiveLists(){
+        // for both stations
+        for(Station station : Arrays.asList(cityStation, univerityStation)){
+            // print the stations list of detectives
+            station.printDetectiveLists();
+        }
+    }
+}
