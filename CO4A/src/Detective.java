07a9340bb6270ab6ec3ebc72d24d91edaa59dc30
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2025-01-21 19:55:36 -0700

    detective class

---
+++ CO4A/src/Detective.java
@@ -0,0 +1,22 @@
+/**
+ *  Detective class
+ *  
+ *  @author Anastasia Sokol
+ *  @author Gray Willits
+ * 
+ *  @date January 21, 2025
+**/
+
+public class Detective {
+    private String name;
+    private int badgeNumber;
+
+    public Detective(String name, int badgeNumber){
+        this.name = name;
+        this.badgeNumber = badgeNumber;
+    }
+
+    public String toString(){
+        return "Detective [Badge=" + badgeNumber + ", Name=" + name + "]";
+    }
+}
