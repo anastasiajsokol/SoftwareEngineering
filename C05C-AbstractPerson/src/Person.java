67ee1fac1cd5452c58882917295d948eed0f936f
Itsgray3 <104292138+Itsgray3@users.noreply.github.com>
2025-01-23 15:23:30 -0700

    Add files via upload

--- C05C-AbstractPerson/src/Person.java
+++ C05C-AbstractPerson/src/Person.java
@@ -1,50 +1,48 @@
 /**
- *  Abstract person class
+ * Abstract person class
  * 
- *  @author Anastasia Sokol
- *  @author Gray Willits
-**/
+ * @author Anastasia Sokol
+ * @author Gray Willits
+ **/
+
+
 public abstract class Person {
-    private String myName;
-    private String occupation;
+	private String myName;
+	private String occupation;
 
-    public Person(String myName, String occupation) {
-        this.myName = myName;
-        this.occupation = occupation;
-    }
+	public Person(String myName, String occupation) {
+		this.myName = myName;
+		this.occupation = occupation;
+	}
 
-    /*
-     * Displays the person asking another person their name
-    */
-    public void askName() {
-        System.out.println("What is your name?");
-    }
+	/*
+	 * Displays the person asking another person their name
+	 */
+	public void askName() {
+		System.out.println("What is your name?");
+	}
 
-    /*
-     * Displays a person giving their name
-    */
-    public void giveName() {
-        System.out.println("My name is " + myName);
-    }
+	/*
+	 * Displays a person giving their name
+	 */
+	public void giveName() {
+		System.out.println("My name is " + myName);
+	}
 
-    /*
-     * Displays the person's occupation
-    */
-    public void whatIDo() {
-        System.out.println("I am a" + ("aeiouAEIOU".contains(occupation.substring(0, 1)) ? "n " : " ") + occupation);
-    }
+	/*
+	 * Displays the person's occupation
+	 */
+	public void whatIDo() {
+		System.out.println("I am a" + ("aeiouAEIOU".contains(occupation.substring(0, 1)) ? "n " : " ") + occupation);
+	}
 
 	/*
 	 * Displays a question
-	*/
-    public abstract void askQuestion();
-    
+	 */
+	public abstract void askQuestion();
+
 	/*
 	 * Displays an answer to a question
-	*/
-    public abstract void answerQuestion();
+	 */
+	public abstract void answerQuestion();
 }
-
-
-
-
