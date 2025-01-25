afb44a414508efc09f5f0af17ddb3246ceecffe0
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2025-01-24 20:11:49 -0700

    update Phineas' responses

--- C05C-AbstractPerson/src/Phineas.java
+++ C05C-AbstractPerson/src/Phineas.java
@@ -1,17 +1,24 @@
+/**
+ *  A person representing Phineas Flynn from the show Phineas and Ferb
+ * 
+ *  @author Anastasia Soko
+ *  @author Gray Willits
+**/
+
 public class Phineas extends Person {
     // from the Phineas and Ferb theme song
     private final String[] THINGS_TO_DO = {
-        "Building a rocket",
-        "Fighting a mummy",
-        "Climbing up the Eiffel Tower",
-        "Discovering something that doesn't exist",
-        "Giving a monkey a shower",
-        "Surfing tidal waves",
-        "Creating nano bots",
-        "Locating Frankenstein's brain",
-        "Finding a dodo bird",
+        "building a rocket",
+        "fighting a mummy",
+        "climbing up the Eiffel Tower",
+        "discovering something that doesn't exist",
+        "giving a monkey a shower",
+        "surfing tidal waves",
+        "creating nano bots",
+        "locating Frankenstein's brain",
+        "finding a dodo bird",
         // what is this missing item?
-        "Driving our sister insane",
+        "driving our sister insane",
     };
 
     // from the running gag throughout the show
@@ -22,24 +29,38 @@ public class Phineas extends Person {
         "building a submarine",
     };
 
+    // levels of annoyance at being asked
+    private final String[] YES = {
+        "Yes, yes I am, thanks for asking",
+        "Yes, yes I am.",
+        "Yes.",
+        "YES!",
+        "YES, WHY DO YOU KEEP LETTING ME GET AWAY WITH THIS!?",
+    };
+
     // counters for THINGS_TO_DO and A_LITTLE_YOUNG respectively
     private int todo;
     private int nextQuestion;
+
+    // make Phineas get more annoyed the more times you ask
+    private int timesAskedAboutAge;
 
     /**
      *  Create a new person representing Phineas Flynn
     **/
     public Phineas(){
         super("Phineas Flynn", "Junior Engineer");
-        todo = 0;
-        nextQuestion = 0;
+        todo = nextQuestion = timesAskedAboutAge = 0;
     }
 
     @Override
     public void whatIDo(){
         // choose the next thing from the theme song (loop back around)
-        System.out.println(THINGS_TO_DO[todo % THINGS_TO_DO.length]);
+        String thingToDo = THINGS_TO_DO[todo % THINGS_TO_DO.length];
         todo += 1;
+
+        // format into message
+        System.out.printf("Finding a good way to spend the summer, like %s.\n", thingToDo);
     }
 
     @Override
@@ -54,7 +75,10 @@ public class Phineas extends Person {
 
     @Override
     public void answerQuestion(){
-        // this is always Phineas' response
-        System.out.println("Yes, yes we are.");
+        // choose an answer based on how many times he has been asked
+        int level = Math.min(timesAskedAboutAge, YES.length - 1);
+        timesAskedAboutAge += 1;
+
+        System.out.println(YES[level]);
     }
 }
