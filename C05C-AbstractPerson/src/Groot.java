b1ba9b654ea5076a3446ec6df32bd347fcb26202
Gray <Gray@10.0.0.57>
2025-01-24 20:25:06 -0700

    Created Groot class

---
+++ C05C-AbstractPerson/src/Groot.java
@@ -0,0 +1,21 @@
+
+public class Groot extends Person {
+
+	public Groot(String myName, String occupation) {
+		super(myName, occupation);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void askQuestion() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void answerQuestion() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
